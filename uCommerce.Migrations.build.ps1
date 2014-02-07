afea994935f9cbf8b2c9424f8206cacdcec051d1
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-02-07 11:04:16 +0100

    include scripts in solution and format them

--- uCommerce.Migrations.build.ps1
+++ uCommerce.Migrations.build.ps1
@@ -1,30 +1,30 @@
 $Global:base_dir = resolve-path .
 
 properties {
-    $configuration = 'Debug'
+	$configuration = 'Debug'
 	$release_path = "$Global:base_dir\release"
 }
 
 task default -depends Clean, Compile, Deploy
 
 task Clean {
-    exec { msbuild .\uCommerce.Migrations.sln /t:clean /p:configuration=$configuration /m }
-    Remove-Item $release_path -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+	exec { msbuild .\uCommerce.Migrations.sln /t:clean /p:configuration=$configuration /m }
+	Remove-Item $release_path -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 }
 
 task Compile {
-    exec { msbuild .\uCommerce.Migrations.sln /p:configuration=$configuration /m }
+	exec { msbuild .\uCommerce.Migrations.sln /p:configuration=$configuration /m }
 }
 
 task Deploy {
-    $release_folders = Ensure-Release-Folders  $release_path
+	$release_folders = Ensure-Release-Folders  $release_path
 }
 
 function Ensure-Release-Folders($base)
 {
-    $release_folders = ($base, "$base\content\")
+	$release_folders = ($base, "$base\content\")
 
-    foreach ($f in $release_folders) { md $f -Force | Out-Null }
+	foreach ($f in $release_folders) { md $f -Force | Out-Null }
 
-    return $release_folders
+	return $release_folders
 }
