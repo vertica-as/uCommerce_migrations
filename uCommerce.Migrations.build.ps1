77879bccf709ca83b8c39a0dd127b39e8412f7d6
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-02-07 10:55:43 +0100

    add Deploy task and ensure release_paths

--- uCommerce.Migrations.build.ps1
+++ uCommerce.Migrations.build.ps1
@@ -5,7 +5,7 @@ properties {
 	$release_path = "$Global:base_dir\release"
 }
 
-task default -depends Clean, Compile
+task default -depends Clean, Compile, Deploy
 
 task Clean {
     exec { msbuild .\uCommerce.Migrations.sln /t:clean /p:configuration=$configuration /m }
@@ -14,5 +14,17 @@ task Clean {
 
 task Compile {
     exec { msbuild .\uCommerce.Migrations.sln /p:configuration=$configuration /m }
+}
 
+task Deploy {
+    $release_folders = Ensure-Release-Folders  $release_path
+}
+
+function Ensure-Release-Folders($base)
+{
+    $release_folders = ($base, "$base\content\")
+
+    foreach ($f in $release_folders) { md $f -Force | Out-Null }
+
+    return $release_folders
 }
